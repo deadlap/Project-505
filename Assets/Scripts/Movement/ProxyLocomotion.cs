2c2ec394c5b2701d25c0544531dd4c925e7dd8fc
deadlap <llabildgaard@hotmail.com>
2024-10-01 19:28:45 +0200

    stuff

--- Assets/Scripts/Movement/ProxyLocomotion.cs
+++ Assets/Scripts/Movement/ProxyLocomotion.cs
@@ -1,17 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class ProxyLocomotion : MonoBehaviour {
 
     //Player Objects
     [SerializeField] private GameObject LeftHand;
     [SerializeField] private GameObject RightHand;
+
+    [SerializeField] private InputActionProperty LeftJoystickInput;
     private Transform HeadTransform;
     private CharacterController PlayerCharacterController;
+    [SerializeField] Transform ForwardTransform;
 
     // Settings 
-
     [SerializeField] float SwingThreshold; //A minimum value for how much the arms should swing to move the user
     [SerializeField] float MovementSpeed; //How fast the user should move;
     [SerializeField] float Smoothness;
@@ -26,25 +28,28 @@ public class ProxyLocomotion : MonoBehaviour {
         PreviousRightHandPosition = RemoveXCoordinate(RightHand.transform.position);
 
         PlayerCharacterController = GetComponent<CharacterController>();
-        HeadTransform = Camera.main.transform;
         MotionVector = Vector3.zero;
     }
 
     void Update() {
-        
+        // var leftHandStickValue = LeftJoystickInput.action?.ReadValue<Vector2>() ?? Vector2.zero;
+        // Vector3 leftHandValue = Vector3.Cross(new Vector3(leftHandStickValue.x, 0, leftHandStickValue.y), ForwardTransform.forward);
+        // Debug.Log(leftHandValue);
+
         //We only want to see direction in the y and z axis to check if the user is swinging their arms.
         
         Vector3 currentLeftHandPosition = RemoveXCoordinate(LeftHand.transform.position);
         Vector3 currentRightHandPosition = RemoveXCoordinate(RightHand.transform.position);
+
         float leftHandVelocity = (currentLeftHandPosition-PreviousLeftHandPosition).magnitude;
         float rightHandVelocity = (currentRightHandPosition-PreviousRightHandPosition).magnitude;
-        // Debug.Log("Right hand: " + rightHandVelocity);
-        // Debug.Log("Left hand: " + leftHandVelocity);
 
+        // if (leftHandVelocity >= SwingThreshold && rightHandVelocity >= SwingThreshold && leftHandStickValue.magnitude > 0.25) {
         if (leftHandVelocity >= SwingThreshold && rightHandVelocity >= SwingThreshold) {
-            // PlayerCharacterController.Move(HeadTransform.forward * MovementSpeed * Time.deltaTime);
 
-            MotionVector = HeadTransform.forward * MovementSpeed * Time.deltaTime;
+            MotionVector = ForwardTransform.forward * MovementSpeed * Time.deltaTime;
+
+            // MotionVector = leftHandValue * MovementSpeed * Time.deltaTime;
         } else {
             MotionVector = Vector3.Lerp(MotionVector, Vector3.zero, Smoothness);
         }
@@ -55,7 +60,7 @@ public class ProxyLocomotion : MonoBehaviour {
         PreviousRightHandPosition = currentRightHandPosition;
     }
 
-    public Vector3 RemoveXCoordinate(Vector3 inputVector) {
+    private Vector3 RemoveXCoordinate(Vector3 inputVector) {
         return new Vector3(0, inputVector.y, inputVector.z);
     }
 }
