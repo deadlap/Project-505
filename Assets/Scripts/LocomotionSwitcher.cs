175863b5567032d40134914518fd6e6b5a6e58a3
deadlap <llabildgaard@hotmail.com>
2024-10-24 20:53:25 +0200

    Skubber til jakob

--- Assets/Scripts/LocomotionSwitcher.cs
+++ Assets/Scripts/LocomotionSwitcher.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class LocomotionSwitcher : MonoBehaviour {
     [SerializeField] ProxyLocomotion proxyLocomotion;
     [SerializeField] int locomotionType;
+    [SerializeField] GameObject Light;
     void OnTriggerEnter(Collider other) {
         Debug.Log("hej");
         if (other.CompareTag("Hand")) {
             proxyLocomotion.ChangeLocomotionType(locomotionType);
+            Light.SetActive(true);
         }
+    }
+    void Update(){
+        Light.SetActive(false);
     }
 }
