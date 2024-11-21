b897bd8451637e752d84bb3f38a3da91442211cd
Akaidus <nikolajjensen6854@gmail.com>
2024-11-21 18:01:28 +0100

    Early visual welding added

--- Assets/Scripts/WeldingMiniGame/WeldingPoint.cs
+++ Assets/Scripts/WeldingMiniGame/WeldingPoint.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WeldingPoint : MonoBehaviour {
-    // Start is called before the first frame update
+    
+    [Header("Create Weld")]
+    [SerializeField] GameObject weldPrefab;
+    [SerializeField] float weldHeight = 0.01f;
+    [SerializeField] float minScale, maxScale;
+    [SerializeField] bool testWeld;
+
     public bool IsFixed {get; private set;}
     void Start() {
         IsFixed = false;
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    // For testing purposes.
+    void Update()
+    {
+        if (!testWeld) return;
+        SpawnWeld();
+    }
+
+    void SpawnWeld() {
+        // Create a weld with a random size.
+        if (IsFixed) return;
+        GameObject newWeld = Instantiate(weldPrefab, transform.position, Quaternion.identity);
+        newWeld.transform.rotation = Quaternion.Euler(new Vector3(90,0,0));
+        newWeld.transform.localScale = new Vector3(Random.Range(minScale, maxScale), weldHeight, Random.Range(minScale, maxScale));
+        IsFixed = true;            
     }
     // void Fix(){
     //     //Run Funny animations;
@@ -19,7 +39,7 @@ public class WeldingPoint : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("WeldingTip") && (other.gameObject.GetComponent<WeldingContact>().WelderGun.Activated)){
-            IsFixed = true;            
+            SpawnWeld();
         }
     }
 }
