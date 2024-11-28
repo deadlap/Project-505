1fabb012357f0e442f9da2f6f95818a27123a5d9
Akaidus <nikolajjensen6854@gmail.com>
2024-11-28 19:22:54 +0100

    intro animation kinda done

---
+++ Assets/Scripts/Sounds/PlayAudio.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
+    void Awake()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+        if (audioClip == null)
+            audioClip = GetComponent<AudioSource>().clip;
+    }
+
+    public void Play(AudioClip ac)
+    {
+        if(audioClip != null)
+            audioSource.PlayOneShot(ac);
+        else
+            audioSource.PlayOneShot(ac);
+    }
+}
