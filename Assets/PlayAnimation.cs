da1d0c3a57ff5f08e31f49cb9179902cb1a7d6d2
Akaidus <nikolajjensen6854@gmail.com>
2024-11-29 03:05:31 +0100

    ny animation

--- Assets/PlayAnimation.cs
+++ Assets/PlayAnimation.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class PlayAnimation : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] bool isCollider;
 
+    void Play()
+    {
+        //animator.SetBool("Play");
+    }
     void OnTriggerEnter(Collider other)
     {
+        if(!isCollider) return;
         if(other.CompareTag("Player"))
             animator.SetBool("ScareTrigger", true);
     }
