5512b565f8e084a995fe01108774e4508071645b
DeadPieGamer <felix@krog.dk>
2024-12-02 12:52:57 +0100

    PlayAudio now has a PlayVaried to add variation to audioclips

--- Assets/Scripts/Sounds/PlayAudio.cs
+++ Assets/Scripts/Sounds/PlayAudio.cs
@@ -5,19 +5,44 @@ public class PlayAudio : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip audioClip;
+    [SerializeField, Tooltip("PlayVaried prioritises playing a random of these over the singular audioClip")] private AudioClip[] multiClips;
+
+    [Header("PlayVaried Audio Settings")]
+    [SerializeField, Range(0f, 1f)] private float minVol = 1f;
+    [SerializeField, Range(0f, 1f)] private float maxVol = 1f;
+    [SerializeField, Range(0f, 2f)] private float minPitch = 1f;
+    [SerializeField, Range(0f, 2f)] private float maxPitch = 1f;
+
     void Awake()
     {
         if (audioSource == null)
             audioSource = GetComponent<AudioSource>();
         if (audioClip == null)
-            audioClip = GetComponent<AudioSource>().clip;
+            audioClip = audioSource.clip;
     }
 
     public void Play(AudioClip ac)
     {
-        if(audioClip != null)
+        if (audioClip != null)
             audioSource.PlayOneShot(ac);
         else
             audioSource.PlayOneShot(ac);
+    }
+
+    public void PlayVaried(AudioClip ac)
+    {
+        audioSource.volume = UnityEngine.Random.Range(minVol, maxVol);
+        audioSource.pitch = UnityEngine.Random.Range(minPitch, maxPitch);
+
+        audioSource.clip = ac;
+        audioSource.Play();
+    }
+
+    public void PlayVaried()
+    {
+        AudioClip ac;
+        if (multiClips != null) ac = multiClips[UnityEngine.Random.Range(0, multiClips.Length)];
+        else ac = audioClip;
+        PlayVaried(ac);
     }
 }
