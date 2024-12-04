11464176ded3e4b999a839846997d8446491b3b0
deadlap <llabildgaard@hotmail.com>
2024-12-04 22:02:11 +0100

    Update EnterDivingBellBox.cs

--- Assets/Scripts/EnterDivingBellBox.cs
+++ Assets/Scripts/EnterDivingBellBox.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class EnterDivingBellBox : MonoBehaviour {
+    float MaxTime;
+    float CurrentTime;
     bool HasTriggered;
     void Start(){
+        MaxTime = 0f;
+        MaxTime = 0.5f;
         HasTriggered = false;
     }
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player") && GameEventManager.INSTANCE.BrokenReached && !HasTriggered) {
-            HasTriggered = true;
-            GameEventManager.INSTANCE.StartEndSequence();
+            CurrentTime += Time.deltaTime;
         }
+    }
+    void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player") && GameEventManager.INSTANCE.BrokenReached && !HasTriggered) {
+            CurrentTime += Time.deltaTime;
+            if (CurrentTime >= MaxTime) {
+                HasTriggered = true;
+                GameEventManager.INSTANCE.StartEndSequence();
+            }
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        CurrentTime = 0;
     }
 }
